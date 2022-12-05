fefc6444063656e3092417c7ad9c43b5c51466f2
ikac22 <ikan2001@gmail.com>
2022-12-05 22:26:51 +0100

    figured out the problem with TCB::yield in idle thread --cleaned

--- build/src/user/syscall_c.d
+++
@@ -1,9 +0,0 @@
-build/src/user/syscall_c.o: src/user/syscall_c.cpp \
- src/user/../../lib/hw.h src/user/../../h/_thread.h \
- src/user/../../h/_semaphore.h
-
-src/user/../../lib/hw.h:
-
-src/user/../../h/_thread.h:
-
-src/user/../../h/_semaphore.h:
